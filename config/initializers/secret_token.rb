6e9e84c187f4aaf9f08d18b81c19090a8f86ac65
Supreeth Burji <supreethjburji@yahoo.com>
2013-03-03 15:11:04 +0530

    adding intial cut without changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyLearn::Application.config.secret_token = '1139b8413d01a4a2cc14c857e92591b2c0fe1a84670ebed2424c5a48823075715f4df45008dda3e7a80d17eaefc354b290136ba703e537ce6f266bc08f94dd7c'
